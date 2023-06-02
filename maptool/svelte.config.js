e6f481ec7af5fbbe71447c5d4f7c22852c72209f
zeyus <zeyus@zeyus.com>
2023-06-02 19:40:19 +0200

    route config for CF

--- maptool/svelte.config.js
+++ maptool/svelte.config.js
@@ -12,11 +12,15 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: 'index.html',
+			// precompress: false,
+			// strict: true,
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
 		  }),
 		paths: {
 			base: '',
@@ -24,10 +28,6 @@ const config = {
 		output: {
 			preloadStrategy: 'preload-mjs',
 	  	},
-		routes: {
-			include: ['/*'],
-			exclude: ['<all>']
-		}
 	}
 };
 
